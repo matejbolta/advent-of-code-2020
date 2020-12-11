ebbe0e62659ce2e0dfb4d140961f6a50756ca810
Matej Bolta <matej.bolta@gmail.com>
2020-12-11 14:10:14 +0100

    day 10, part 2 solved

--- 2020/src/day10.ml
+++ 2020/src/day10.ml
@@ -20,22 +20,14 @@ let one_three_difference input =
 
 
 let naloga1 content = (* 1904 on my input *)
-  content |> one_three_difference |> string_of_int
+  content |> one_three_difference
 (*  *)
 
 
+(* Slovarji za memoizacijo *)
 type 'a tree = Empty | Node of 'a tree * 'a * 'a tree (* trees will be used as dictionaries *)
 (*  *)
 let leaf x = Node (Empty, x, Empty)
-(*  *)
-let rec print_dict dict = match dict with
-  | Empty -> ()
-  | Node (l, (k, v), d) ->
-    print_dict l; (* ; vzame 2 unit expresiona in izvede oba, vrne unit *)
-    print_string (string_of_int k ^ " : ");
-    print_endline(String.concat " " (List.map string_of_int v));
-    (* print_string (k ^ " : "); print_int v; print_newline (); *)
-    print_dict d
 (*  *)
 let rec dict_get key dict =
   match dict with
@@ -83,55 +75,41 @@ let build_dictionary input = (* Very procedural *) (* int list -> (int * int lis
 (*  *)
 let option_type_to_x dict_opt last_element = function
   | Some x -> x
-  | None ->
-    print_dict dict_opt;
-    print_endline (string_of_int last_element ^ " to je tale ključ");
-    failwith "option type je none"
+  | None -> failwith "option type je none"
+(*  *)
+let dict_memo = ref Empty
+  (* MEMOIZACIJA - za shranjevanje memoiziranih counterjev možnosti *)
+  (* je referenca, ker jo aktivno spreminjamo in je globalna spremenljivka *)
 (*  *)
 let rec number_of_options last_element dict_opt input =
-  let dict_memo = ref Empty in (* za shranjevanje memoiziranih števil možnosti od m *)
-  let rec aux () =
-      let get_memo (x: int) dict_opt =
-        let resit = dict_get x !dict_memo in
-        match resit with
-        | Some r -> r
-        | None ->
-          let r = number_of_options x dict_opt input in
-          let _ = (dict_memo := (dict_insert x r !dict_memo)) in
-          r
-      in
-    let possible : (int list) = option_type_to_x dict_opt last_element (dict_get last_element dict_opt) in
-    let nth i = List.nth possible i in
-    let len = List.length possible in
-    if len = 3 then
-      get_memo (nth 0) dict_opt + get_memo (nth 1) dict_opt + get_memo (nth 2) dict_opt
-    else if len = 2 then
-      get_memo (nth 0) dict_opt + get_memo (nth 1) dict_opt
-    else if len = 1 then
-      get_memo (nth 0) dict_opt
-    else 1 (* possible = [] *)
+  let get_memo (x: int) dict_opt =
+    let resit = dict_get x !dict_memo in
+    match resit with
+    | Some r -> r
+    | None ->
+      let r = number_of_options x dict_opt input in
+      let _ = (dict_memo := (dict_insert x r !dict_memo)) in
+      r
   in
-  aux ()
+  let possible : (int list) = option_type_to_x dict_opt last_element (dict_get last_element dict_opt) in
+  let nth i = List.nth possible i in
+  let len = List.length possible in
+  if len = 3 then
+    get_memo (nth 0) dict_opt + get_memo (nth 1) dict_opt + get_memo (nth 2) dict_opt
+  else if len = 2 then
+    get_memo (nth 0) dict_opt + get_memo (nth 1) dict_opt
+  else if len = 1 then
+    get_memo (nth 0) dict_opt
+  else 1 (* possible = [] *)
 (*  *)
 
 
+let naloga2 content = (* MEMOIZACIJA *)
+  let l = content in
+  let input = (0 :: l) @ [(List.hd (List.rev l)) + 3] in (* doda 0 na začetek in max+3 na konec seznama *)
+  let dict_opt = build_dictionary input in (* slovar možnosti za n-je, 'naslednike' od m *)
 
-
-let input = [0;1;2;5;6;9;10;11;12;13;16;19;20;21;22;23;26;27;28;29;30;33;34;35;36;37;40;41;42;43;46;47;48;51;54;55;58;59;62;63;64;65;68;69;70;73;76;77;78;81;82;83;84;87;90;91;92;93;94;97;98;99;100;103;106;107;108;109;112;113;114;115;116;119;120;121;122;123;126;127;128;129;132;133;134;135;138;139;140;141;142;145;146;147;148;149;152]
-(*  *)
-let dict_opt = (build_dictionary input) (* slovar možnosti za n-je, naslednike od m *)
-
-
-(* let r = number_of_options 0 !dict_opt input *)
-(* 02:02 start *)
-
-
-
-
-let naloga2 content = "unsolved"
-  (* let l = content in *)
-  (* let input = (0 :: l) @ [(List.hd (List.rev l)) + 3] in *)
-
+  number_of_options 0 !dict_opt input (* prvo število je vedno 0, dict_opt je vendno enak *)
 (*  *)
 
 
@@ -155,11 +133,19 @@ let _ =
     |> List.sort compare
   in
 
-  let s1 = naloga1 content in
-  let s2 = naloga2 content in
+  let s1 = naloga1 content |> string_of_int in
+  let s2 = naloga2 content |> string_of_int in
   
   write_file ("2020/out/day_" ^ day ^ "_1.out") s1;
   print_endline ("day " ^ day ^ ", puzzle 1: " ^ s1);
   write_file ("2020/out/day_" ^ day ^ "_2.out") s2;
   print_endline ("day " ^ day ^ ", puzzle 2: " ^ s2)
 (*  *)
+
+(*
+Dan 11:
+Prva naloga je bila vaja iz uporabe zank in referenc, kar se mi zdi kar lepo.
+Druga naloga je bila kombinacija referenc in uporabe -memoizacije- v ocamlu.
+Mučna izkušnja. Morda je malo proceduralno, ampak vseeno kar lepo.
+Implementiral sem tudi slovarje kot search treeje kar je kr najs :)
+*)
